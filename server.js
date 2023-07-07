361e608605c6ac3f5ee6d80aa496ace375eb374e
kritika10120 <122134592+kritika10120@users.noreply.github.com>
2023-07-08 02:17:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 /***************************
+*  WEB322 – Assignment 04
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: __Kritika Kritika_____ Student ID: __167103217_____ Date: 07-07-2023
+*  Cyclic Web App URL: 
+*
+*  GitHub Repository URL: 
+*
+
+
+/***************************
 *  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
@@ -8,6 +20,8 @@
 *
 *  GitHub Repository URL: ______https://github.com/kritika10120/web322-app____________
 *
+****************************/
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -15,7 +29,12 @@ const fs = require('fs');
 const app = express();
 const PORT = 5500;
 
-// Serve the categories.html file with categories data
+// Serve the about.html file
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/about.html'));
+});
+
+// Serve the categories.json file
 app.get('/categories', (req, res) => {
   const categoriesPath = path.join(__dirname, 'data/categories.json');
   fs.readFile(categoriesPath, 'utf8', (err, data) => {
@@ -24,16 +43,12 @@ app.get('/categories', (req, res) => {
       res.status(500).send('Internal Server Error');
     } else {
       const categories = JSON.parse(data);
-      let categoriesHTML = '<h1>Categories</h1>';
-      categories.forEach((category) => {
-        categoriesHTML += `<p>${category}</p>`;
-      });
-      res.send(categoriesHTML);
+      res.json(categories);
     }
   });
 });
 
-// Serve the posts.html file with posts data
+// Serve the posts.json file
 app.get('/posts', (req, res) => {
   const postsPath = path.join(__dirname, 'data/posts.json');
   fs.readFile(postsPath, 'utf8', (err, data) => {
@@ -42,18 +57,19 @@ app.get('/posts', (req, res) => {
       res.status(500).send('Internal Server Error');
     } else {
       const posts = JSON.parse(data);
-      let postsHTML = '<h1>Posts</h1>';
-      posts.forEach((post) => {
-        postsHTML += `<p>${post}</p>`;
-      });
-      res.send(postsHTML);
+      res.json(posts);
     }
   });
 });
 
-// Serve the index.html file for all other routes
+// Serve the addPost.html file
+app.get('/posts/add', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/addPost.html'));
+});
+
+// Serve the about.html file for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(path.join(__dirname, 'views/about.html'));
 });
 
 // Start the server
