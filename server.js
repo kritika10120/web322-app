71bb59fca738a48dd99d5845f69c70ca92a23f01
kritika10120 <122134592+kritika10120@users.noreply.github.com>
2023-07-08 02:00:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,66 +1,175 @@
-/***************************
-*  WEB322 – Assignment 03
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
-*  (including 3rd party web sites) or distributed to other students.
-* 
-*  Name: __Kritika Kritika_____ Student ID: __167103217_____ Date: 08-06-2023
-*  Cyclic Web App URL: _________https://web322appassignment03.cyclic.app/___________
-*
-*  GitHub Repository URL: ______https://github.com/kritika10120/web322-app____________
-*
-****************************/
-
+const exphbs = require('express-handlebars');
 const express = require('express');
-const path = require('path');
-const fs = require('fs');
-
+const blogData = require("./blog-service");
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const streamifier = require('streamifier');
+const path = require("path");
 const app = express();
-const PORT = 5500;
 
-// Serve the about.html file
+const HTTP_PORT = process.env.PORT || 8080;
+app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+const upload = multer();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect("/about");
+});
+
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/about.html'));
+    res.sendFile(path.join(__dirname, "/views/about.html"))
 });
 
-// Serve the categories.json file
-app.get('/categories', (req, res) => {
-  const categoriesPath = path.join(__dirname, 'data/categories.json');
-  fs.readFile(categoriesPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      const categories = JSON.parse(data);
-      res.json(categories);
+app.get('/blog', async (req, res) => {
+
+    // Declare an object to store properties for the view
+    let viewData = {};
+
+    try{
+
+        // declare empty array to hold "post" objects
+        let posts = [];
+
+        // if there's a "category" query, filter the returned posts by category
+        if(req.query.category){
+            // Obtain the published "posts" by category
+            posts = await blogData.getPublishedPostsByCategory(req.query.category);
+        }else{
+            // Obtain the published "posts"
+            posts = await blogData.getPublishedPosts();
+        }
+
+        // sort the published posts by postDate
+        posts.sort((a,b) => new Date(b.postDate) - new Date(a.postDate));
+
+        // get the latest post from the front of the list (element 0)
+        let post = posts[0]; 
+
+        // store the "posts" and "post" data in the viewData object (to be passed to the view)
+        viewData.posts = posts;
+        viewData.post = post;
+
+    }catch(err){
+        viewData.message = "no results";
     }
-  });
-});
 
-// Serve the posts.json file
-app.get('/posts', (req, res) => {
-  const postsPath = path.join(__dirname, 'data/posts.json');
-  fs.readFile(postsPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      const posts = JSON.parse(data);
-      res.json(posts);
+    try{
+        // Obtain the full list of "categories"
+        let categories = await blogData.getCategories();
+
+        // store the "categories" data in the viewData object (to be passed to the view)
+        viewData.categories = categories;
+    }catch(err){
+        viewData.categoriesMessage = "no results"
     }
-  });
+
+    // render the "blog" view with all of the data (viewData)
+    res.render("blog", {data: viewData})
+
+});
+app.get('/posts', (req,res)=>{
+
+    let queryPromise = null;
+
+    if(req.query.category){
+        queryPromise = blogData.getPostsByCategory(req.query.category);
+    }else if(req.query.minDate){
+        queryPromise = blogData.getPostsByMinDate(req.query.minDate);
+    }else{
+        queryPromise = blogData.getAllPosts()
+    } 
+
+    queryPromise.then(data=>{
+        res.render("categories", {categories: data});
+    }).catch(err=>{
+        res.json({message: err});
+    })
+
 });
 
-// Serve the addPost.html file
-app.get('/posts/add', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/addPost.html'));
+app.post("/posts/add", upload.single("featureImage"), (req,res)=>{
+
+    if(req.file){
+        let streamUpload = (req) => {
+            return new Promise((resolve, reject) => {
+                let stream = cloudinary.uploader.upload_stream(
+                    (error, result) => {
+                        if (result) {
+                            resolve(result);
+                        } else {
+                            reject(error);
+                        }
+                    }
+                );
+    
+                streamifier.createReadStream(req.file.buffer).pipe(stream);
+            });
+        };
+    
+        async function upload(req) {
+            let result = await streamUpload(req);
+            console.log(result);
+            return result;
+        }
+    
+        upload(req).then((uploaded)=>{
+            processPost(uploaded.url);
+        });
+    }else{
+        processPost("");
+    }
+
+    function processPost(imageUrl){
+        req.body.featureImage = imageUrl;
+
+        blogData.addPost(req.body).then(post=>{
+            res.redirect("/posts");
+        }).catch(err=>{
+            res.status(500).send(err);
+        })
+    }   
 });
 
-// Serve the about.html file for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/about.html'));
+app.get('/posts/add', (req,res)=>{
+   res.sendFile(path.join(__dirname, "/views/addPost.html"));
+}); 
+
+app.get('/post/:id', (req,res)=>{
+    blogData.getPostById(req.params.id).then(data=>{
+        res.json(data);
+    }).catch(err=>{
+        res.json({message: err});
+    });
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get('/categories', (req,res)=>{
+    blogData.getCategories().then((data=>{
+        res.json(data);
+    })).catch(err=>{
+        res.json({message: err});
+    });
+});
+
+app.use((req,res)=>{
+    res.status(404).send("404 - Page Not Found")
+})
+
+blogData.initialize().then(()=>{
+    app.listen(HTTP_PORT, () => { 
+        console.log('server listening on: ' + HTTP_PORT); 
+    });
+}).catch((err)=>{
+    console.log(err);
+})
+const stripJs = require('strip-js');
+
+exphbs.create({
+  helpers: {
+    safeHTML: function (context) {
+      return stripJs(context);
+    },
+  },
 });
