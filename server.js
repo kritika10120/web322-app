06f546a4e6e78a5d5d40794f6fbb0bca15d50826
kritika10120 <122134592+kritika10120@users.noreply.github.com>
2023-07-08 04:00:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 /***************************
-*  WEB322 – Assignment 04
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: __Kritika Kritika_____ Student ID: __167103217_____ Date: 07-07-2023
-*  Cyclic Web App URL: 
+*  Name: __Kritika Kritika_____ Student ID: __167103217_____ Date: 08-06-2023
+*  Cyclic Web App URL: _________https://web322appassignment03.cyclic.app
 *
-*  GitHub Repository URL: https://github.com/kritika10120/web322-app
+*  GitHub Repository URL: ______https://github.com/kritika10120/web322-app____________
 *
+****************************/
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 8080;
+const PORT = 5500;
 
 // Serve the about.html file
 app.get('/about', (req, res) => {
@@ -29,20 +31,7 @@ app.get('/categories', (req, res) => {
       res.status(500).send('Internal Server Error');
     } else {
       const categories = JSON.parse(data);
-      const tableRows = categories.map(category => {
-        return `<tr>
-          <td>${category.id}</td>
-          <td>${category.name}</td>
-        </tr>`;
-      });
-      const table = `<table>
-        <tr>
-          <th>Category ID</th>
-          <th>Category Name</th>
-        </tr>
-        ${tableRows.join('')}
-      </table>`;
-      res.send(table);
+      res.json(categories);
     }
   });
 });
@@ -56,26 +45,7 @@ app.get('/posts', (req, res) => {
       res.status(500).send('Internal Server Error');
     } else {
       const posts = JSON.parse(data);
-      const tableRows = posts.map(post => {
-        return `<tr>
-          <td>${post.id}</td>
-          <td>${post.title}</td>
-          <td>${post.date}</td>
-          <td>${post.category}</td>
-          <td>${post.published}</td>
-        </tr>`;
-      });
-      const table = `<table>
-        <tr>
-          <th>Post ID</th>
-          <th>Title</th>
-          <th>Post Date</th>
-          <th>Category</th>
-          <th>Published</th>
-        </tr>
-        ${tableRows.join('')}
-      </table>`;
-      res.send(table);
+      res.json(posts);
     }
   });
 });
